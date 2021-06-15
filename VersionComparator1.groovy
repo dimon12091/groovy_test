5bd9692eb56176f7fc645de62d0bb5020c9c98db
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-15 17:32:13 +0300

    commit

--- VersionComparator1.groovy
+++ VersionComparator1.groovy
@@ -1,3 +1,4 @@
+@NonCPS
 List versions = [ '02.2.02.01', '02.2.02.02', '02.2.03.01']
 String mostRecentVersion = versions.sort {a, b ->
   def a1 = a.tokenize('.')*.toInteger(), b1 = b.tokenize('.')*.toInteger()
@@ -10,6 +11,14 @@ String mostRecentVersion = versions.sort {a, b ->
 }[-1]
 
 assert mostRecentVersion == '02.2.03.01'
+
+
+// versions = versions.sort {a, b ->
+//   def a1 = a.tokenize('.')*.toInteger(), b1 = b.tokenize('.')*.toInteger()
+//   for (i in 0..<[a1.size(), b1.size()].min())
+//     if (a1[i] != b1[i]) return a1[i] <=> b1[i]
+//   0
+// }
 
 
 //
