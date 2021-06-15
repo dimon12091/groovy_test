ce9e20a40a4b3612768850172d7036bdeed00fbd
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-15 17:17:36 +0300

    commit

--- VersionComparator1.groovy
+++ VersionComparator1.groovy
@@ -10,3 +10,26 @@ String mostRecentVersion = versions.sort {a, b ->
 }[-1]
 
 assert mostRecentVersion == '02.2.03.01'
+
+
+//
+// def recurse
+// def versionArraySort = { a1, a2 ->
+//     def headCompare = a1[0] <=> a2[0]
+//     if (a1.size() == 1 || a2.size() == 1 || headCompare != 0) {
+//         return headCompare
+//     } else {
+//         return recurse(a1[1..-1], a2[1..-1])
+//     }
+// }
+// // fool Groovy to understand recursive closure
+// recurse = versionArraySort
+//
+// def versionStringSort = { s1, s2 ->
+//     def nums = { it.tokenize('.').collect{ it.toInteger() } }
+//     versionArraySort(nums(s1), nums(s2))
+// }
+//
+// versions = ["1.10.0","1.9.10","1.9.9","1.9.11"]
+// println versions.sort()
+// println versions.sort(versionStringSort)
