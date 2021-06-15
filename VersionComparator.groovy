d2eac3cd17e8578c2345282bf866b8b719e40bef
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-15 16:28:23 +0300

    first commit

---
+++ VersionComparator.groovy
@@ -0,0 +1,37 @@
+def versions = []
+def f = new File('mock-version-tags.txt')
+f.eachLine { versions << it }
+
+def versionComparator = { a, b ->
+  def VALID_TOKENS = /._/
+  a = a.tokenize(VALID_TOKENS)
+  b = b.tokenize(VALID_TOKENS)
+
+  for (i in 0..<Math.max(a.size(), b.size())) {
+    if (i == a.size()) {
+      return b[i].isInteger() ? -1 : 1
+    } else if (i == b.size()) {
+      return a[i].isInteger() ? 1 : -1
+    }
+
+    if (a[i].isInteger() && b[i].isInteger()) {
+      int c = (a[i] as int) <=> (b[i] as int)
+      if (c != 0) {
+        return c
+      }
+    } else if (a[i].isInteger()) {
+      return 1
+    } else if (b[i].isInteger()) {
+      return -1
+    } else {
+      int c = a[i] <=> b[i]
+      if (c != 0) {
+        return c
+      }
+    }
+  }
+
+  return 0
+}
+
+versions.sort(versionComparator)
