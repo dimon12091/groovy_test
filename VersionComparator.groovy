fad241f1a1d703bf470bc14741b5d89d3399ccc4
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-16 19:32:16 +0300

    commit

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -1,32 +1,20 @@
 array = ["3.8.10","3.9.2","3.7.dublicate.5","3.9.1","3.8.3","3.8.2","2.7.2","2.7.1","3.8.1","3.7.4","3.7.3","3.6.5","3.6.4", "<none>", "1.19.9-fix", "1.19.9-fix2"
 ]
-
-
-  for(i=array.size()-1;i>1;i--){
-   for(j=0;j<i;j++){
-   if (array[j+1] == "<none>" ){
-    array.swap(j,j+1)
-   }
-    b = array[j].tokenize('.')[2]
-    c = array[j+1].tokenize('.')[2]
-    println(b.size())
-    if ((array[j][0..2] == array[j+1][0..2] && b > c) || (array[j][0..2] == array[j+1][0..2] && b < c) || (b.size() > 2 || c.size() > 2 )) {
+for(i=array.size()-1;i>1;i--){
+    for(j=0;j<i;j++){
+        if (myList[j+1] !== "<none>" ) {
         array.swap(j,j+1)
-        continue
+        }
+        b = array[j].tokenize('.')[2]
+        c = array[j+1].tokenize('.')[2]
+        if ((array[j][0..2] == array[j+1][0..2] && b > c) || (array[j][0..2] == array[j+1][0..2] && b < c) || (b.size() > 2 || c.size() > 2 )) {
+            array.swap(j,j+1)
+            continue
+        }
+        if(array[j][0..2]>array[j+1][0..2]){
+            array.swap(j,j+1)
+        }
     }
-    if(array[j][0..2]>array[j+1][0..2]){
-        array.swap(j,j+1)
-    }
-   }
-  }
-
+}
 
 println(array)
-
-list = ["<none>", "bash"]
-if (list[0] == "<none>") {
-    println(list[0])
-}
-else (
-    println("you are stupid")
-)
