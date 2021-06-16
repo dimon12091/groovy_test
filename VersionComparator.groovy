754fe80442b4d1071af41bd2a80498df2c19fa85
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-16 13:36:29 +0300

    commit

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -19,6 +19,8 @@ def bubbleSort(array) {
     def VALID_TOKENS = /._/
     b = array[j].tokenize(VALID_TOKENS)[2]
     c = array[j+1].tokenize(VALID_TOKENS)[2]
+    println(b)
+    println(b)
     if(array[j][0..2]>=array[j+1][0..2] && b > c){
      array.swap(j,j+1)
     }
