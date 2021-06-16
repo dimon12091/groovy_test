fcb6dfca65fbda61a4037ae9d1b98e9b88a32fe0
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-16 15:55:41 +0300

    commit

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -1,5 +1,5 @@
 array = ["3.8.10","3.9.2","3.9.1","3.8.3","3.8.2","2.7.2","2.7.1","3.8.1","3.7.4","3.7.3","3.6.5","3.6.4", "1.19.9-fix", "1.19.9-fix2"]
-def bubbleSort(array) {
+app = def bubbleSort(array) {
   for(i=array.size()-1;i>1;i--){
    for(j=0;j<i;j++){
     def VALID_TOKENS = /._/
@@ -14,7 +14,6 @@ def bubbleSort(array) {
     }
    }
   }
-  return List array
 }
 bubbleSort(array)
-println(array.array)
+println(app)
