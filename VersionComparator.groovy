aa8b394e04f1d7c7a74893d2c402e06f584b3c71
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-16 17:42:04 +0300

    commit

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -4,9 +4,6 @@ array = ["3.8.10","3.9.2","3.9.1","3.8.3","3.8.2","2.7.2","2.7.1","3.8.1","3.7.4
    for(j=0;j<i;j++){
     b = array[j].tokenize('.')[2]
     c = array[j+1].tokenize('.')[2]
-    if (array[j] = "<none>") {
-        array.swap(-1, 0)
-    }
     if ((array[j][0..2] == array[j+1][0..2] && b > c) || (array[j][0..2] == array[j+1][0..2] && b < c)) {
         array.swap(j,j+1)
         continue
@@ -19,3 +16,11 @@ array = ["3.8.10","3.9.2","3.9.1","3.8.3","3.8.2","2.7.2","2.7.1","3.8.1","3.7.4
 
 
 println(array)
+
+List = ["<none>", "bash"]
+if (List[0] == "<none") {
+    println(List[0])
+}
+else (
+    println("you are stupid")
+)
