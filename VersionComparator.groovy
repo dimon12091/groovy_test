cb90aa36d88f1898890f0cba558c2430e651e4c5
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-16 18:23:55 +0300

    commit

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -4,14 +4,17 @@ array = ["3.8.10","3.9.2","3.7.dublicate.5","3.9.1","3.8.3","3.8.2","2.7.2","2.7
 
   for(i=array.size()-1;i>1;i--){
    for(j=0;j<i;j++){
+   if (array[j+1] == "<none>" ){
+    array.swap(j,j+1)
+   }
     b = array[j].tokenize('.')[2]
     c = array[j+1].tokenize('.')[2]
     println(b.size())
-    if ((array[j][0..2] == array[j+1][0..2] && b > c) || (array[j][0..2] == array[j+1][0..2] && b < c) || (array[j+1] == "<none>" ) || (b.size() > 2 || c.size() > 2 )) {
+    if ((array[j][0..2] == array[j+1][0..2] && b > c) || (array[j][0..2] == array[j+1][0..2] && b < c) || (b.size() > 2 || c.size() > 2 )) {
         array.swap(j,j+1)
         continue
     }
-    if((array[j][0..2]>array[j+1][0..2]) || (array[j+1] == "<none>" )){
+    if(array[j][0..2]>array[j+1][0..2]){
         array.swap(j,j+1)
     }
    }
