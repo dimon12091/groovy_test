2d92adadefb381c6ad8de58036c5090d06daa48f
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-16 18:17:25 +0300

    commit

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -6,7 +6,8 @@ array = ["3.8.10","3.9.2","3.9.1","3.8.3","3.8.2","2.7.2","2.7.1","3.8.1","3.7.4
    for(j=0;j<i;j++){
     b = array[j].tokenize('.')[2]
     c = array[j+1].tokenize('.')[2]
-    if ((array[j][0..2] == array[j+1][0..2] && b > c) || (array[j][0..2] == array[j+1][0..2] && b < c) || (array[j+1] == "<none>" ) || (b.size > 2 || c.size > 2 )) {
+    println(b.size())
+    if ((array[j][0..2] == array[j+1][0..2] && b > c) || (array[j][0..2] == array[j+1][0..2] && b < c) || (array[j+1] == "<none>" ) || (b.size() > 2 || c.size() > 2 )) {
         array.swap(j,j+1)
         continue
     }
