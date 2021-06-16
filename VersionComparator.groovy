710dfc3a08ec706f08efddb3f1f8601412ebcd4c
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-16 12:08:10 +0300

    commit

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -1,16 +1,40 @@
-def f = ["1.0.12", "1.0.2", "0.1",
-"0.1.0.2","0.2", "2.0", "0.2.0.4",
-"0.2.0.3.1", "1.5.2_05", "1.5.2_04", "1.5.2_10","1.6.0_01","2.0.0_02", "1.0RC2", "1.0RC1","3.1"]
+def f = ["3.8.10",
+"3.9.2",
+"3.9.1",
+"3.8.3",
+"3.8.2",
+"2.7",
+"2.7.1",
+"3.8.1",
+"3.7.4",
+"3.7.3",
+"3.6.5",
+"3.6.4",
+]
 
 for (x in f) {
     println(x)
     def VALID_TOKENS = /._/
-    x = x.tokenize(VALID_TOKENS)
-    println(x)
+    x1 = x.tokenize(VALID_TOKENS)
+    println(x1)
 }
-def versionComparator = { a, b->
-  def VALID_TOKENS = /._/
-  a = a.tokenize(VALID_TOKENS)
-  b = b.tokenize(VALID_TOKENS)
+
+def bubbleSort(String[] array) {
+  for(i=array.size()-1;i>1;i--){
+   for(j=0;j<i;j++){
+    if(array[j]>array[j+1]){
+     swap(j,j+1)
+    }
+   }
+  }
 }
-f.sort(versionComparator)
+
+void swap(String[] array,int one,int two)
+{
+ def temp = array[one]
+ array[one] = array[two]
+ array[two] = temp
+}
+
+bubbleSort(f)
+
