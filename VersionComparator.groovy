d735f91ffe615b4e35bf7756c79a91ead3b37bb5
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-16 11:22:26 +0300

    commit

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -4,40 +4,15 @@ def f = ["1.0.12", "1.0.2", "0.1",
 
 println (f)
 @NonCPS
-def versionComparator = { a, b ->
+def versionComparator = { a, b, c, d ->
   def VALID_TOKENS = /._/
   a = a.tokenize(VALID_TOKENS)
   b = b.tokenize(VALID_TOKENS)
+  c = c.tokenize(VALID_TOKENS)
+  d = d.tokenize(VALID_TOKENS)
 
   println(a)
   println(b)
 
-  for (i in 0..<Math.max(a.size(), b.size())) {
-    if (i == a.size()) {
-      return b[i].isInteger() ? -1 : 1
-
-    } else if (i == b.size()) {
-      return a[i].isInteger() ? 1 : -1
-    }
-    if (a[i].isInteger() && b[i].isInteger()) {
-      int c = (a[i] as int) <=> (b[i] as int)
-      if (c != 0) {
-        return c
-      }
-    } else if (a[i].isInteger()) {
-      return 1
-    } else if (b[i].isInteger()) {
-      return -1
-    } else {
-      int c = a[i] <=> b[i]
-      if (c != 0) {
-        return c
-      }
-    }
-  }
-  println(c)
-
-  return 0
-}
 
 f.sort(versionComparator)
