6c9fc4b07479e93289478e8e410d759ff2cef290
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-16 11:33:10 +0300

    commit

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -3,8 +3,7 @@ def f = ["1.0.12", "1.0.2", "0.1",
 "0.2.0.3.1", "1.5.2_05", "1.5.2_04", "1.5.2_10","1.6.0_01","2.0.0_02", "1.0RC2", "1.0RC1","3.1"]
 
 println (f)
-@NonCPS
-for x in f {
+for (x in f) {
     def VALID_TOKENS = /._/
     x = x.tokenize(VALID_TOKENS)
 }
