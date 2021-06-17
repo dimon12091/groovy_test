9dcc35b461060d47c55cef32802f2e2788bb78ab
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-17 12:13:36 +0300

    asdasd

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -10,6 +10,10 @@ array = ["3.8.10","3.9.2","3.7.dublicate.5","3.9.1","1.17.5","1.17.5-alpine", "1
                 array.swap(j,j+1)
                 continue
             }
+            if ()(array[j][0..3] == array[j+1][0..3]) && (array[j+1].size > array[j].size )){
+                 array.swap(j,j+1)
+                 continue
+            }
             if((array[j] != "<none>") && (array[j+1] != "<none>") && (array[j][0..2]<array[j+1][0..2]) && (array[j+1].tokenize('.')[2].size() < 3 ) && (array[j].size() < 8 )){
                 array.swap(j,j+1)
                 continue
