69318630a5d94f6f75ac93f276760ed31c4b703e
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-17 14:46:10 +0300

    asdasd

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -1,31 +1,36 @@
-array = ["3.8.10","3.9.2","3.7.dublicate.5","3.9.1","1.17.5","1.17.5-alpine","<none>", "1.19.9-fix", "1.19.9-fix2" , "1.13.tempCheckTaskComplete2"]
-for(i=array.size()-1;i>1;i--){
-                for(j=0;j<i;j++){
-
-                    if (array[j] == "<none>"){
-                        array.swap(j,j+1)
-                        continue
-                    }
-
-                    if ( (array[j] != "<none>") && (array[j+1] != "<none>") && (array[j].tokenize('.')[2].size() > 2) ) {
-                        array.swap(j,j+1)
-                        continue
-                    }
-
-                    if ((array[j] != "<none>") && (array[j+1] != "<none>") && (array[j][0..3] < array[j+1][0..3]) && (array[j].size() < array[j+1].size() )){
-                        array.swap(j,j+1)
-                        continue
-                    }
-
-                    if ((array[j][0..3] == array[j+1][0..3]) && (array[j].size() < array[j+1].size() )){
-                        array.swap(j,j+1)
-                        continue
-                    }
-
+// array = ["3.8.10","3.9.2","3.7.dublicate.5","3.9.1","1.17.5","1.17.5-alpine","<none>", "1.19.9-fix", "1.19.9-fix2" , "1.13.tempCheckTaskComplete2"]
+// for(i=array.size()-1;i>1;i--){
+//                 for(j=0;j<i;j++){
+//
+//                     if (array[j] == "<none>"){
+//                         array.swap(j,j+1)
+//                         continue
+//                     }
+//
+//                     if ( (array[j] != "<none>") && (array[j+1] != "<none>") && (array[j].tokenize('.')[2].size() > 2) ) {
+//                         array.swap(j,j+1)
+//                         continue
+//                     }
+//
+//                     if ((array[j] != "<none>") && (array[j+1] != "<none>") && (array[j][0..3] < array[j+1][0..3]) && (array[j].size() < array[j+1].size() )){
+//                         array.swap(j,j+1)
+//                         continue
+//                     }
+//
+//                     if ((array[j][0..3] == array[j+1][0..3]) && (array[j].size() < array[j+1].size() )){
+//                         array.swap(j,j+1)
+//                         continue
+//                     }
+//
 //                     if((array[j] != "<none>") && (array[j+1] != "<none>") && (array[j][0..2]<array[j+1][0..2]) && (array[j+1].tokenize('.')[2].size() < 3 ) && (array[j].size() < 8 )){
 //                         array.swap(j,j+1)
 //                         continue
 //                     }
-                }
-            }
-println(array)
+//                 }
+//             }
+// println(array)
+
+
+list = ["1.14"]
+list.put(list[0], ".0")
+println(list)
