fe646e82f22a478954009f8ff89ad7198c084021
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-17 13:37:28 +0300

    asdasd

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -1,29 +1,31 @@
-array = ["3.8.10","3.9.2","3.7.dublicate.5","3.9.1","1.17.5","1.17.5-alpine","<none>", "1.19.9-fix", "1.19.9-fix2" , "1.13.tempCheckTaskComplete2"]
+// array = ["3.8.10","3.9.2","3.7.dublicate.5","3.9.1","1.17.5","1.17.5-alpine","<none>", "1.19.9-fix", "1.19.9-fix2" , "1.13.tempCheckTaskComplete2"]
+//
+//     for(i=array.size()-1;i>1;i--){
+//         for(j=0;j<i;j++){
+//             if (array[j] == "<none>"){
+//                 array.swap(j,j+1)
+//                 continue
+//             }
+//             if ((array[j].tokenize('.')[2].size() > 2) && (array[j+1] != "<none>" ) ) {
+//                 array.swap(j,j+1)
+//                 continue
+//             }
+//             if ((array[j][0..3] == array[j+1][0..2]) && (array[j+1].size() > array[j].size() )){
+//                  array.swap(j,j+1)
+//                  continue
+//             }
+//             if ((array[j][0..3] < array[j+1][0..2]) && (array[j].size() < array[j+1].size() )){
+//                  array.swap(j,j+1)
+//                  continue
+//             }
+//
+//             if((array[j] != "<none>") && (array[j+1] != "<none>") && (array[j][0..2]<array[j+1][0..2]) && (array[j+1].tokenize('.')[2].size() < 3 ) && (array[j].size() < 8 )){
+//                 array.swap(j,j+1)
+//                 continue
+//             }
+//         }
+//     }
+// println(array)
 
-    for(i=array.size()-1;i>1;i--){
-        for(j=0;j<i;j++){
-            if (array[j] == "<none>"){
-                array.swap(j,j+1)
-                continue
-            }
-            if ((array[j].tokenize('.')[2].size() > 2) && (array[j+1] != "<none>" ) ) {
-                array.swap(j,j+1)
-                continue
-            }
-            if ((array[j][0..2] < array[j+1][0..2]) && (array[j].size() < array[j+1].size() )){
-                 array.swap(j,j+1)
-                 continue
-            }
-            if ((array[j][0..2] == array[j+1][0..2]) && (array[j+1].size() > array[j].size() )){
-                 array.swap(j,j+1)
-                 continue
-            }
-            if((array[j] != "<none>") && (array[j+1] != "<none>") && (array[j][0..2]<array[j+1][0..2]) && (array[j+1].tokenize('.')[2].size() < 3 ) && (array[j].size() < 8 )){
-                array.swap(j,j+1)
-                continue
-            }
-        }
-    }
-println(array)
-println("1.13.tempCheckTaskComplete2"[0..3])
-println("1.13.asdsad.tempCheckTaskComplete2"[0..3])
+nums = []
+println(nums.size()
