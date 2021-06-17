09a5119a17c8dd506e7d2bf9b612ef024c3d8331
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-17 18:05:00 +0300

    asdasd

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -34,3 +34,4 @@ app = ["1.19.9-fix", "1.19.9-fix2", "1.14.4", "  1.17.1.mocked_rules_assignment"
 println(app[2].tokenize('.')[4] == null)
 println(app[0].tokenize('.')[2][0])
 println( app[3].tokenize('.')[3] != null)
+println("1.19.8" > "1.20")
