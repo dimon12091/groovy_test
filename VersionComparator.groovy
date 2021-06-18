f3da7b92558da609c3592c6b9b0dfc5e1234e43b
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-18 15:23:35 +0300

    asdasd

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -34,13 +34,13 @@ println(test[-2].tokenize('.')[2].indexOf('-'))
 println(test.getClass().getSimpleName())
 println(test[3].matches("(?i).*[a-zа-я].*"))
 println(test[0].matches("(?i).*[a-zа-я].*"))
-// for(i=test.size()-1;i>1;i--){
-//     for(j=0;j<i;j++){
-//         println(test)
-//         if ((test[j].tokenize('.')[2].indexOf('-') != 1) && (test[j+1].tokenize('.')[2].indexOf('-') != 1)) {
-//             if ((test[j][0..3] <= test[j+1][0..3]) && (test[j].tokenize('.')[2].toInteger() < test[j+1].tokenize('.')[2].toInteger())){
-//                 test.swap(j,j+1)
-//             }
-//         }
-//     }
-// }
+for(i=test.size()-1;i>1;i--){
+    for(j=0;j<i;j++){
+        println(test)
+        if ((test[j].tokenize('.')[2].indexOf('-') != 1) && (test[j+1].tokenize('.')[2].indexOf('-') != 1) && (test[j+1].tokenize('.')[2].matches("(?i).*[a-zа-я].*"))){
+            if ((test[j][0..3] <= test[j+1][0..3]) && (test[j].tokenize('.')[2].toInteger() < test[j+1].tokenize('.')[2].toInteger())){
+                test.swap(j,j+1)
+            }
+        }
+    }
+}
