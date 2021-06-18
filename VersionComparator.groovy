c3d0d6a38980606353eca201cd45356c6c7ee461
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-18 17:34:37 +0300

    asdasd

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -30,8 +30,8 @@ pipeline {
 }
 
 test = [ "1.20.2", "1.20.1", "1.19.9-fix2", "1.19.10-fix", "1.19.20", "1.19.11", "1.19.10", "1.19.9"]
-println(test[2].tokenize('.')[2][0].toInteger())
-println(test[3].tokenize('.')[2][0].toInteger())
+println(test[2].tokenize('.')[2].split('-'))
+println(test[3].tokenize('.')[2].split('-')
 println(test[0].matches("(?i).*[a-zа-я].*"))
 for(i=test.size()-1;i>1;i--){
     for(j=0;j<i;j++){
