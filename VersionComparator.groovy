cbf5e74673ac4544f686ec91022024c46b047567
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-18 13:59:34 +0300

    asdasd

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -18,13 +18,13 @@ test = [ "1.19.8", "1.18.8", "1.18.9", "1.19.23"]
 println(test[-1].tokenize('.')[2])
 for(i=test.size()-1;i>1;i--){
     for(j=0;j<i;j++){
-        if (myList[j][0..3] == true && myList[j+1][0..3] == true ){
-            if ((myList[j][0..3] == myList[j+1][0..3]) && (myList[j].tokenize('.')[2] > myList[j+1].tokenize('.')[2])){
-                myList.swap(j,j+1)
+        if (test[j][0..3] == true && test[j+1][0..3] == true ){
+            if ((test[j][0..3] == test[j+1][0..3]) && (test[j].tokenize('.')[2] > test[j+1].tokenize('.')[2])){
+                test.swap(j,j+1)
                 continue
             }
-            if ((myList[j][0..3] < myList[j+1][0..3]) && (myList[j].tokenize('.')[2] > myList[j+1].tokenize('.')[2])){
-                myList.swap(j,j+1)
+            if ((test[j][0..3] < test[j+1][0..3]) && (test[j].tokenize('.')[2] > test[j+1].tokenize('.')[2])){
+                test.swap(j,j+1)
                 continue
             }
         }
