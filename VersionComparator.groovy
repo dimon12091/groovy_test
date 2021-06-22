237af22446e4482682599b03630711c497987cc4
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-22 19:48:51 +0300

    modified file

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -17,13 +17,13 @@ pipeline {
 app = ["1.9", "1.20.1", "1.19.9-fix2", "1.19.9-fix", "1.19.20", "1.19.11", "1.19.10", "1.19.9"]
 for ( x in app) {
 
-    if (x.tokenize(".")[2].trim().length == 0) {
+    if (x.tokenize(".")[2].length == 0) {
         println('hi')
     }
-    if (x.tokenize(".")[2].trim().length() == false) {
+    if (x.tokenize(".")[2].length() == false) {
         println('hi1')
     }
-    if (x.tokenize(".")[2].trim().length() == true) {
+    if (x.tokenize(".")[2].length() == true) {
         println('hi1')
     }
 }
