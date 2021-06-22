a64e1cae7d54265a9d7c074702212e6dceea825c
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-22 19:08:52 +0300

    modified file

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -15,7 +15,7 @@ pipeline {
     }
 }
 app = ["1.9", "1.20.1", "1.19.9-fix2", "1.19.9-fix", "1.19.20", "1.19.11", "1.19.10", "1.19.9"]
-if (app[0].tokenize.[2] == false) {
+if (app[0].tokenize(".")[2] == false) {
     m = app[0] + ".0"
     println(m)
 }
