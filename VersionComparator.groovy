87921fd2015bcdf9aeed4f275d90baaa8eeee143
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-22 10:39:13 +0300

    asdasd

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -31,6 +31,7 @@ pipeline {
 app = ["9-fix2", "10-fix2"]
 println(app[0].split('-')[0])
 println(app[1].split('-')[0])
+println(app[1].split('-')[0].toInteger > app[0].split('-')[0].toInteger )
 // test = [ "1.20.2", "1.20.1", "1.19.9-fix2", "1.19.10-fix", "1.19.20", "1.19.11", "1.19.10", "1.19.9"]
 // println(test[3].tokenize('.')[2].split("-"))
 // println(test[0].matches("(?i).*[a-zа-я].*"))
