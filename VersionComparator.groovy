f159feb7c692996d26e5315f5ec4aee3388adae6
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-23 15:20:43 +0300

    modified file

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -14,19 +14,18 @@ pipeline {
         }
     }
 }
-app = ["1.9", "1.20.1", "1.19.9-fix2", "1.19.9-fix", "1.19.20", "1.19.11", "1.19.10", "1.19.9"]
-for ( x in app) {
+// app = ["1.9", "1.20.1", "1.19.9-fix2", "1.19.9-fix", "1.19.20", "1.19.11", "1.19.10", "1.19.9"]
+// for ( x in app) {
+//
+//     if (x.tokenize(".")[2].equals(null) == true) {
+//         println('hi')
+//     }
+//     if (x.tokenize(".")[2].equals(null)  == false) {
+//         println('hi1')
+//     }
+// }
 
-    if (x.tokenize(".")[2].equals(null) == true) {
-        println('hi')
-    }
-    if (x.tokenize(".")[2].equals(null)  == false) {
-        println('hi1')
-    }
-}
 
-
-test = ["1.9", "1.20.1"]
-println(test.size())
-println(app[2].tokenize(".")[1] == app[3].tokenize(".")[1])
-println(app[2].tokenize(".")[2] == app[3].tokenize(".")[2])
+test = []
+test.add("1.19")
+println(test)
