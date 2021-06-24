1a6f3ac993c38a947b2e5515f8b0b2cbbcbb1603
Dmytro Boiko <dmytro12091@gmail.com>
2021-06-24 17:52:05 +0300

    modified file

--- VersionComparator.groovy
+++ VersionComparator.groovy
@@ -15,7 +15,7 @@ pipeline {
     }
 }
 myList = ['1.20.4', '1.20.3', '1.20.2', '1.20.1', '1.19.duplicate_actions.5',  '1.19.duplicate_actions.4', '1.19.duplicate_actions.3', '1.19.duplicate_actions.2', '1.19.duplicate_actions.1', '1.19.8', '1.19.5', '1.19.4', '1.19.23', '1.19.2', '1.19.19', '1.19.17', '1.18.9', '1.18.8', '1.18.4', '1.18.3', '1.18.2', '1.18.1', '1.17.9.mocked_rules_assignment', '1.17.8.mocked_rules_assignment', '1.17.27', '1.17.23', '1.17.1.mocked_rules_assignment', '1.17.10.mocked_rules_assignment', '1.17.04', '1.16.estate-for-web', '1.16.9', '1.16.7', '1.16.6', '1.16.5', '1.16.4.estate-for-web', '1.16.3.estate-for-web', '1.15.fix_report_generation_02', '1.14.testDocumentExplorer9', '1.14.testDocumentExplorer8']
-println(myList[0].tokenize(".")[0] + '.' +myList[0].tokenize(".")[1] < myList[0])
+println(myList[0].tokenize(".")[0] + '.' +myList[0].tokenize(".")[1] == )
 for(i=myList.size()-1;i>1;i--){
             for(j=0;j<i;j++){
 
@@ -59,44 +59,13 @@ for(i=myList.size()-1;i>1;i--){
                      continue
                 }
             }
-    head_tag = myList[0]
+
+}
+println(myList)
+head_tag = myList[0]
     rmList = []
     var = 0
     control_tag = "0.0.0"
-    if (head_tag != "" ) {
-        for (tag in myList) {
-            if (tag != "<none>") {
-                if (tag.tokenize(".")[2].equals(null) == true){
-                    tag = tag + ".0"
-                }
-                if (tag.tokenize(".")[0] == head_tag.tokenize(".")[0] && tag.tokenize(".")[1] == head_tag.tokenize(".")[1]) {
-                    continue
-                }
-                if (var > 2 ) {
-                    rmList.add(tag)
-                    continue
-                }
-
-                if ( (tag.tokenize(".")[0] == head_tag.tokenize(".")[0] && tag.tokenize(".")[1] == head_tag.tokenize(".")[1]) && (tag.tokenize(".")[2].split("-")[0].toInteger() <= control_tag.tokenize(".")[2].split("-")[0].toInteger()) ) {
-                    rmList.add(tag)
-                    continue
-                }
-
-                if ( tag > tag.tokenize(".")[0] + '.' + myList[0].tokenize(".")[1]) && tag < head_tag ) {
-                    control_tag = tag
-
-                    var = var + 1
-                }
-            }
-        }
-    }
-}
-println(myList)
-
-head_tag = myList[0]
-rmList = []
-var = 0
-control_tag = "0.0.0"
     if (head_tag != "" ) {
         for (tag in myList) {
             if (tag != "<none>") {
